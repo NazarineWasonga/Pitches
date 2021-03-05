2f774de5b0090295463a8c541a8c4b9270853499
NazarineWasonga <nazarinewasonga48@gmail.com>
2021-03-05 16:00:27 +0300

    modified app/forms.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
-app = Flask(__name__)
-app.config.from_object(Config)
+app = Flask(__name__)  
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 from app import routes, models
--- app/forms.py
+++ app/forms.py
@@ -5,27 +5,27 @@ from app.models import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[
-                           DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField(
-        'Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+        username = StringField('Username', validators=[
+        DataRequired(), Length(min=2, max=20)])
+        email = StringField('Email', validators=[DataRequired(), Email()])
+        password = PasswordField('Password', validators=[DataRequired()])
+        confirm_password = PasswordField(
+            'Confirm Password', validators=[DataRequired(), EqualTo('password')])
+        submit = SubmitField('Sign Up')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different username.')
+        def validate_username(self, username):
+            user = User.query.filter_by(username=username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
 
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different email address.')
+        def validate_email(self, email):
+            user = User.query.filter_by(email=email.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different email address.')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+        email = StringField('Email', validators=[DataRequired()])
+        password = PasswordField('Password', validators=[DataRequired()])
+        remember_me = BooleanField('Remember Me')
+        submit = SubmitField('Sign In')
