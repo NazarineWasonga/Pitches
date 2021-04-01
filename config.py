89c104a2cf7208814e752ecabc25a6d8e3296b24
NazarineWasonga <nazarinewasonga48@gmail.com>
2021-04-01 12:27:14 +0300

    This commit fixes bugs

--- config.py
+++ config.py
@@ -1,38 +1,48 @@
-
 import os
-
-
+from dotenv import load_dotenv
+load_dotenv()
 class Config:
-    """
-    Parent config
-    """
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ['SECRET_KEY']
+    DEBUG=True
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SECRET_KEY='nazarine'
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    # email configurations
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = 'nazarinewasonga48@gmail.com'
-    MAIL_PASSWORD = 'naz48810'
-
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
 
 class ProdConfig(Config):
-    """
-    Production config
-    """
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
 
 class DevConfig(Config):
-    """
-    Development config
-    """
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:naz48810/nazarine'
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://nazarine:nazryn01@:5432/pitches'
+
     DEBUG = True
-
-
+    ENV = 'development'
+    
 config_options = {
-    'development': DevConfig,
-    'production': ProdConfig
-}
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
